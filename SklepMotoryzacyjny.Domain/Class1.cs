1801623e94b80b379d3a4d6c0db9ba3613cf349b
KacperSierakowski <218608@student.pwr.edu.pl>
2018-07-17 01:02:01 +0200

    START

---
+++ SklepMotoryzacyjny.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SklepMotoryzacyjny.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ SklepMotoryzacyjny.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SklepMotoryzacyjny.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SklepMotoryzacyjny.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df6bfb10-6a85-4437-be1d-d822695fed9b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
