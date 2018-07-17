f310aaf6345530a0223b19db02c28000f6ada413
KacperSierakowski <218608@student.pwr.edu.pl>
2018-07-17 19:41:53 +0200

    Usuwanie elementów z koszyka

--- SklepMotoryzacyjny.WebUI/Infrastructure/Binders/CartModelBinder.cs
+++ SklepMotoryzacyjny.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -1,9 +1,4 @@
 ﻿using SportsStore.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.ModelBinding;
 using System.Web.Mvc;
 
 namespace SklepMotoryzacyjny.WebUI.Infrastructure.Binders
